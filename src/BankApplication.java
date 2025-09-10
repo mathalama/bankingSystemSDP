398af9619576c60e4c9795ee1e984d9f0ba9953d
Aikyn <ajkynsagyntaj@gmail.com>
2025-09-10 21:29:59 +0500

    builder

---
+++ src/BankApplication.java
@@ -0,0 +1,12 @@
+public class BankApplication {
+    public static void main(String[] args) {
+        User aikyn = User.builder()
+                .setFullName("Aikyn")
+                .setEmail("aikyn@mathalama.dev")
+                .setIIN("112233888999")
+                .setNumber("87771238888")
+                .setPassword("math123lama")
+                .build();
+        System.out.println(aikyn);
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,74 @@
+public class User {
+    private final String fullName;
+    private final String email;
+    private final String IIN;
+    private final String number;
+    private final String password;
+
+    public User(Builder builder) {
+        this.fullName = builder.fullName;
+        this.password = builder.password;
+        this.email = builder.email;
+        this.IIN = builder.IIN;
+        this.number = builder.number;
+
+    }
+
+    public static class Builder {
+        private String fullName;
+        private String password;
+        private String email;
+        private String IIN;
+        private String number;
+
+        public Builder setFullName(String fullName) {
+            if (fullName == null || fullName.isEmpty()) {
+                throw new NullPointerException("Name cannot be empty");
+            }
+            this.fullName = fullName;
+            return this;
+        }
+        public Builder setEmail(String email) {
+            if (email == null || !email.contains("@")) {
+                throw new NullPointerException("Invalid email");
+            }
+            this.email = email;
+            return this;
+        }
+        public Builder setPassword(String password) {
+            if (password == null || password.isEmpty()) {
+                throw new NullPointerException("Password cannot be empty");
+            }
+            this.password = password;
+            return this;
+        }
+        public Builder setIIN(String IIN) {
+            if (IIN != null && IIN.isEmpty() || !IIN.chars().allMatch(Character::isDigit) || IIN.length() != 12) {
+                throw new IllegalArgumentException("Invalid IIN: must contain only numbers");
+            }
+            this.IIN = IIN;
+            return this;
+        }
+        public Builder setNumber(String number) {
+            if (number == null || !number.matches("(\\+7|8)\\d{10}")) {
+                throw new IllegalArgumentException("Invalid phone number");
+            }
+            this.number = number;
+            return this;
+        }
+        public User build() {
+            return new User(this);
+        }
+    }
+    public static Builder builder() {
+        return new Builder();
+    }
+    @Override
+    public String toString() {
+        return "Full Name = " + fullName + "\n" +
+                "IIN: " + IIN + "\n" +
+                "Number: " + number + "\n" +
+                "Email = " + email + "\n" +
+                "Password = i can't display the password";
+    }
+}
